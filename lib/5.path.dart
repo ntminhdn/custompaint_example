4816c16fd6b5cc282225b258cdeb35e82cb4dba1
NALS_MACBOOK_342 <nals_macbook_342@NALS-MACBOOK-342s-MacBook-Pro.local>
2023-03-19 16:40:18 +0700

    ref

---
+++ lib/5.path.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('5. Path'),
+        ),
+        backgroundColor: Colors.black,
+        body: Center(
+          child: CustomPaint(
+            size: const Size(300, 300),
+            painter: MyCustomPainter(),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+class MyCustomPainter extends CustomPainter {
+  final redPaint = Paint()
+    ..color = Colors.red
+    ..strokeWidth = 2;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final width = size.width;
+    final height = size.height;
+
+    print(Offset(3, 4).distance);
+
+    final path = Path()
+      ..moveTo(width / 2, 0)
+      ..lineTo(width, height)
+      ..lineTo(0, height)
+      ..close();
+
+    canvas.drawPath(path, redPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/6.bubble_chat.dart
+++ lib/6.bubble_chat.dart
@@ -39,7 +39,8 @@ class _ChatScreenState extends State<ChatScreen> {
           Expanded(
             child: ListView.builder(
               itemCount: _messages.length,
-              itemBuilder: (context, index) => BubbleChat(message: _messages[index]),
+              itemBuilder: (context, index) =>
+                  BubbleChat(message: _messages[index]),
             ),
           ),
           Row(
@@ -115,30 +116,32 @@ class MyCustomPainter extends CustomPainter {
 
   final bool isMine;
   final Paint bgPaint;
-  static const arrowSize = 10;
+  static const arrowSize = 10.0;
 
   @override
   void paint(Canvas canvas, Size size) {
     final width = size.width;
     final height = size.height;
-    final rightPoint1 = Offset(width, height / 2 - 5);
-    final rightPoint2 = Offset(width + sqrt(pow(10, 2) - pow(5, 2)), height / 2);
+    final rightPoint1 = Offset(width, height / 2 - (arrowSize / 2));
+    final rightPoint2 = Offset(
+        width + sqrt(pow(arrowSize, 2) - pow(arrowSize / 2, 2)), height / 2);
     final centerPoint = Offset(width / 2, height / 2);
-    final leftPoint1 = centerPoint * 2 - rightPoint1;
-    final leftPoint2 = centerPoint * 2 - rightPoint2;
+    final leftPoint1 = centerPoint * 2 - rightPoint1; // đối xứng tâm
+    final leftPoint2 = centerPoint * 2 - rightPoint2; // đối xứng tâm
 
     canvas.drawRRect(
-        RRect.fromRectAndRadius(Rect.fromLTRB(0, 0, width, height), const Radius.circular(10)),
+        RRect.fromRectAndRadius(
+            Rect.fromLTRB(0, 0, width, height), const Radius.circular(10)),
         bgPaint);
     final rightPath = Path()
       ..moveTo(rightPoint1.dx, rightPoint1.dy)
-      ..relativeLineTo(0, 10)
+      ..relativeLineTo(0, arrowSize)
       ..lineTo(rightPoint2.dx, rightPoint2.dy)
-      ..close();
+      ..close(); // ko cần close cũng đc
 
     final leftPath = Path()
       ..moveTo(leftPoint1.dx, leftPoint1.dy)
-      ..relativeLineTo(0, -10)
+      ..relativeLineTo(0, -arrowSize)
       ..lineTo(leftPoint2.dx, leftPoint2.dy);
 
     canvas.drawPath(isMine ? rightPath : leftPath, bgPaint);
